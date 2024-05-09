daf4520c76a92346eebe19a70e06269fdd7c590b
lm106 <lzmps106@outlook.es>
2024-05-09 11:24:07 +0100

    rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TheWelcome from "@/components/welcome/TheWelcome.vue";
+import ContentForm from "@/components/form/content/ContentForm.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Welcome',
+      component: TheWelcome
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/form',
+      name: 'Form',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../components/form/Form.vue'),
+      children: [
+        {
+          path: '/form',
+          name: 'Relevance',
+          component:ContentForm
+        },
+        {
+          path: '/form',
+          name: 'Fair',
+          component:ContentForm
+        },
+        {
+          path: '/form',
+          name: 'Resolution and temporal coverage',
+          component:ContentForm
+        },
+        {
+          path: '/form',
+          name: 'SDQF',
+          component:ContentForm
+        },
+        {
+          path: '/form',
+          name: 'Results',
+          component:ContentForm
+        }
+      ]
     }
   ]
 })
