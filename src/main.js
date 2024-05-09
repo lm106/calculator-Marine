d05fd82611ccfbdba723763f4e9fbe1913058df8
lm106 <lzmps106@outlook.es>
2024-05-09 12:33:51 +0100

    incoporar vuetify

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
