f1e4d4af5dceca974af79e0de391dd4f15c629e9
lm106 <lzmps106@outlook.es>
2024-06-07 17:01:14 +0100

    Organización

--- src/components/form/content/utils.js
+++ src/components/form/content/utils.js
@@ -5,6 +5,27 @@ export function getKey(obj){
     return Object.keys(obj);
     // return (key.length>0)? true: false;
 }
+
+export function getCopy(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.map(getCopy);
+    }
+
+    const clonedObj = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clonedObj[key] = getCopy(obj[key]);
+        }
+    }
+
+    return clonedObj;
+}
+
+/***************************Values***********************/
 export function checkQuestionsStep(keyStep){
     // console.log('check',keyStep)
     return (values.value[keyStep]!=undefined)? true: false;
@@ -92,27 +113,6 @@ function convertToOutput(item) {
     }
 }
 
-
-/************************************/
-
-export function getCopy(obj) {
-    if (obj === null || typeof obj !== 'object') {
-        return obj;
-    }
-
-    if (Array.isArray(obj)) {
-        return obj.map(getCopy);
-    }
-
-    const clonedObj = {};
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            clonedObj[key] = getCopy(obj[key]);
-        }
-    }
-
-    return clonedObj;
-}
 /******************TransformValues*******************/
 export function setFilterValues(){
     transformValues.value ={};
