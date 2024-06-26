88a80f8ce35c2b6067899808d7e7170886a0d4a1
lm106 <lzmps106@outlook.es>
2024-06-26 13:26:10 +0100

    Dashboard de collections. Diseño funcionalidad básica de la lista en la tabla.

--- src/components/reports/content/Collections.vue
+++ src/components/reports/content/Collections.vue
@@ -1,11 +1,43 @@
 <script setup>
 import {useRoute} from "vue-router";
+import {onBeforeMount, ref, watch} from 'vue';
+
+import DashboardCollections from "@/components/reports/content/collection/DashboardCollections.vue";
+
 
 const route= useRoute();
+const list=ref(['List 1', "List 2"])
+
+
+onBeforeMount(() => {
+
+});
+
 </script>
 
 <template>
   {{ route.name }}
+  <v-expansion-panels class="panels panels_report">
+    <v-expansion-panel v-for="(name, i) in list" :key="i"
+                       class="panel_block panel_report" style="border-top-left-radius: 10px !important; border-top-right-radius: 10px !important;
+          border-bottom-left-radius: 10px !important; border-bottom-right-radius: 10px !important;"
+    >
+      <template #title>
+        <div class="title_block">
+          <h3 class="title">{{ name }}</h3>
+          <v-divider></v-divider>
+        </div>
+      </template>
+      <v-expansion-panel-text>
+        <div class="content_block">
+          <v-container class="item_activity">
+            <DashboardCollections :activeCluster="name"></DashboardCollections>
+          </v-container>
+        </div>
+      </v-expansion-panel-text>
+      <v-spacer></v-spacer>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <style scoped>
