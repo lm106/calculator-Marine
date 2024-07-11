4fd181b35cb7881e58f2a9c72ef8659ecdcc1ac8
lm106 <lzmps106@outlook.es>
2024-07-11 14:09:01 +0100

    quitar en el import defineprops

--- src/components/reports/content/collection/DashboardCollections.vue
+++ src/components/reports/content/collection/DashboardCollections.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, computed, defineProps} from 'vue';
+import {ref, computed} from 'vue';
 import {listValues} from "@/variables/store.js";
 import {
   getAccurancyConsistency,
