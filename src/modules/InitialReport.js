164a54ed9319b7e64a31b2d166a7fa48eab4e2f6
lm106 <lzmps106@outlook.es>
2024-07-30 12:15:11 +0100

    Estructura de calcular valores y gráficas.

---
+++ src/modules/InitialReport.js
@@ -0,0 +1,38 @@
+
+import {ref} from "vue";
+import {getOutputValues} from "@/modules/OutputValue.js";
+import {setFilterTransformValues} from "@/modules/TransformValues.js";
+import {setScoreGlobal} from "@/modules/ScoreGlobal.js";
+import {setCountRow} from "@/modules/countRow.js";
+import {
+    setSeriesCategory,
+    setSeriesClusters,
+    setSeriesScoreCategory,
+    setSeriesStepClusters
+} from "@/modules/SeriesCharts.js";
+import {generatePDF} from "@/modules/download.js";
+export const tokenReport=ref(true);
+export function initReport(n) {
+    console.log("initReport");
+    console.log(n);
+    if(tokenReport.value){
+        initReportValues();
+        initReportChart();
+        tokenReport.value=false;
+    }
+}
+
+function initReportValues(){
+    getOutputValues();
+    setFilterTransformValues();
+    setScoreGlobal();
+    setCountRow();
+}
+
+function initReportChart(){
+    setSeriesCategory();
+    setSeriesScoreCategory();
+    setSeriesClusters();
+    setSeriesStepClusters();
+}
+
