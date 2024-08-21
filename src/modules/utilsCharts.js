cfe7a9abe60a147cc73ff8906df5a553169f8404
lm106 <lzmps106@outlook.es>
2024-08-21 10:43:55 +0100

    nombre de todas la categorias del cluster

--- src/modules/utilsCharts.js
+++ src/modules/utilsCharts.js
@@ -9,3 +9,19 @@ export function getCategories(step){
     // })
     return questions[step];
 }
+export function getCategoriesCluster(){
+    return [
+        'Relevance scores',
+        'Relevance for national MSP process',
+        'Considered/used within MSP process',
+        'Fair scores', 'Data is Findable',
+        'Data is Accesible', 'Data is Interoperable',
+        'Data is Reusable', 'Resolution and temporal scores',
+        'Spatial coverage (extension) of the data set',
+        'Spatial resolution of the data (level of details)',
+        'Temporal coverage', 'Temporal resolution', 'SDQF scores',
+        'Completeness and timeliness',
+        'Data accuracy and possible margin of error',
+        'Data consistency with MSP input data',
+    ]
+}
