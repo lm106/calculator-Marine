1c30fdb81bd429d02e8149ac64ad4610cfca4a1f
lm106 <lzmps106@outlook.es>
2024-10-10 08:48:43 +0100

    Refactor: listado de filter of collections

--- src/components/reports/content/collection/DashboardCollections.vue
+++ src/components/reports/content/collection/DashboardCollections.vue
@@ -19,6 +19,7 @@ import {
   getRelevantFulfilling,
   getResolution
 } from "@/rules/rulesList.js";
+import DashboardCluster from "@/components/reports/content/cluster/DashboardCluster.vue";
 
 const props=defineProps({
   activeList:String
@@ -102,53 +103,75 @@ const activeItems = computed(() => generateItems());
 </script>
 
 <template>
-  <v-card flat>
-    <v-card-title class="d-flex align-right pe-2 mb-5">
-      <h4 class="title">{{ props.activeList }}</h4>
-      <v-spacer class="w-75"></v-spacer>
-      <v-text-field
-          v-model="search"
-          density="compact"
-          label="Search"
-          prepend-inner-icon="mdi-magnify"
-          variant="solo-filled"
-          flat
-          hide-details
-          single-line
-          class="w-25"
-      ></v-text-field>
-    </v-card-title>
-
-    <v-divider></v-divider>
-    <v-data-table v-model:search="search" :items="activeItems" :header="headers" height="600px" fixed-header fixed-footer>
-      <template v-slot:headers="{ columns, isSorted, getSortIcon, toggleSort }">
-        <tr class="header_tr">
-          <template v-for="column in columns" :key="column.key">
-            <td>
-              <span class="mr-2 cursor-pointer" @click="() => toggleSort(column)">
-                {{ column.title }}
-              </span>
-              <v-icon class="btn_info_table" color="grey" icon="mdi-information"></v-icon>
-              <template v-if="isSorted(column)">
-                <v-icon :icon="getSortIcon(column)"></v-icon>
-              </template>
-            </td>
-          </template>
-        </tr>
-      </template>
-      <template v-slot:item.name="{ item }">
-        <div class="nameParameter">{{ item.name }}</div>
-      </template>
-      <template  v-for="header in headers.slice(1)" v-slot:[`item.${header.value}`]="{ item }">
-        <div class="text-center">
-          <v-icon :icon="item[header.value] ? 'mdi-check-circle' : 'mdi-close-circle'"
-              size="small" :color="item[header.value] ? 'green' : 'red'">
-<!--              {{ item[header.value] ? '' : 'Out' }}-->
-          </v-icon>
+<!--  <v-card flat>-->
+<!--    <v-card-title class="d-flex align-right pe-2 mb-5">-->
+<!--      <h4 class="title">{{ props.activeList }}</h4>-->
+<!--      <v-spacer class="w-75"></v-spacer>-->
+<!--      <v-text-field-->
+<!--          v-model="search"-->
+<!--          density="compact"-->
+<!--          label="Search"-->
+<!--          prepend-inner-icon="mdi-magnify"-->
+<!--          variant="solo-filled"-->
+<!--          flat-->
+<!--          hide-details-->
+<!--          single-line-->
+<!--          class="w-25"-->
+<!--      ></v-text-field>-->
+<!--    </v-card-title>-->
+<!--    <v-divider></v-divider>-->
+<!--    <v-data-table v-model:search="search" :items="activeItems" :header="headers" height="600px" fixed-header fixed-footer>-->
+<!--      <template v-slot:headers="{ columns, isSorted, getSortIcon, toggleSort }">-->
+<!--        <tr class="header_tr">-->
+<!--          <template v-for="column in columns" :key="column.key">-->
+<!--            <td>-->
+<!--              <span class="mr-2 cursor-pointer" @click="() => toggleSort(column)">-->
+<!--                {{ column.title }}-->
+<!--              </span>-->
+<!--              <v-icon class="btn_info_table" color="grey" icon="mdi-information"></v-icon>-->
+<!--              <template v-if="isSorted(column)">-->
+<!--                <v-icon :icon="getSortIcon(column)"></v-icon>-->
+<!--              </template>-->
+<!--            </td>-->
+<!--          </template>-->
+<!--        </tr>-->
+<!--      </template>-->
+<!--      <template v-slot:item.name="{ item }">-->
+<!--        <div class="nameParameter">{{ item.name }}</div>-->
+<!--      </template>-->
+<!--      <template  v-for="header in headers.slice(1)" v-slot:[`item.${header.value}`]="{ item }">-->
+<!--        <div class="text-center">-->
+<!--          <v-icon :icon="item[header.value] ? 'mdi-check-circle' : 'mdi-close-circle'"-->
+<!--              size="small" :color="item[header.value] ? 'green' : 'red'">-->
+<!--&lt;!&ndash;              {{ item[header.value] ? '' : 'Out' }}&ndash;&gt;-->
+<!--          </v-icon>-->
+<!--        </div>-->
+<!--      </template>-->
+<!--    </v-data-table>-->
+<!--  </v-card>-->
+  <v-expansion-panels class="panels panels_report" multiple>
+    <v-expansion-panel v-for="(name_list, i) in headers.slice(1)" :key="i" class="panel_block panel_report"
+                       style="border-top-left-radius: 10px !important; border-top-right-radius: 10px !important;
+                              border-bottom-left-radius: 10px !important; border-bottom-right-radius: 10px !important;"
+    >
+      <template #title>
+        <div class="title_block">
+          <h3 class="title">{{ name_list.text}}</h3>
+          <v-divider></v-divider>
         </div>
       </template>
-    </v-data-table>
-  </v-card>
+      <v-expansion-panel-text>
+        <div class="content_block">
+          <v-container class="item_activity">
+            <ul>
+            <template v-for="(item,i) in activeItems" ><li v-if="item[name_list.value]">{{ item.name }}</li></template>
+            </ul>
+          </v-container>
+        </div>
+      </v-expansion-panel-text>
+      <v-spacer></v-spacer>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <style scoped>
